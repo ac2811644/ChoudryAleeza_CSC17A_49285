9f9f2ded74752567afe073d012092fb07abbcb9a
ac2811644 <70608686+ac2811644@users.noreply.github.com>
2020-09-01 23:41:20 -0700

    Add files via upload

---
+++ Lab/IntroLab/main.cpp
@@ -0,0 +1,38 @@
+/* 
+ * File:   main.cpp
+ * Author: Aleeza Choudry
+ * Created on September 1, 2020, 7:30 PM
+ * Purpose:  Hello World - Intro Lab Assignment
+ */
+
+//System Libraries
+#include <iostream>  //I/O Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants Only
+//Well known Science, Mathematical and Laboratory Constants
+
+//Function Prototypes
+
+//Execution of Code Begins Here
+int main(int argc, char** argv) {
+    //Set the random number seed here
+    
+    //Declare all variables for this function
+    
+    //Initialize all known variables
+    
+    //Process Inputs to Outputs -> Mapping Process
+    //Maps known values to the unknown objectives
+    
+    //Display the Inputs/Outputs
+    cout<<"Hello World"<<endl;
+
+    //Clean up the code, close files, deallocate memory, etc....
+    //Exit stage right
+    return 0;
+}
+
+//Function Implementations
